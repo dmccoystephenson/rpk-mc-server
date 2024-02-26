ba4087d204e5dbc9d99ffb36fd079f9d95c1e2fd
dmccoystephenson <dmccoystephenson@gmail.com>
2024-02-25 17:54:33 -0700

    Allowed developer to specify operators & plugin-overrides

--- post-create.sh
+++ post-create.sh
@@ -10,10 +10,16 @@ if [ -z "$(ls -A /rpkmcserver)" ]; then
     # Copy JARs
     cp /jars/*.jar /rpkmcserver/plugins
 
+    # Copy config files
+    cp /config/ops.json /rpkmcserver
+
     # Accept EULA
     cd /rpkmcserver && echo "eula=true" > eula.txt
 else
     echo "Server is already set up."
 fi
+
+echo "Copying file overrides..."
+cp /deposit-box/plugin-overrides/*.jar /rpkmcserver/plugins
 
 java -jar /rpkmcserver/spigot-1.19.4.jar
